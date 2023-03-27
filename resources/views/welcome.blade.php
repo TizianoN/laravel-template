050bb1d7dd6f4b6ee44a3ffe08dbfb07193f6ad3
Tiziano <hello@tizianonicolai.com>
2023-03-27 20:00:16 +0200

    fix logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
 <body>
   <main>
     <div class="container d-flex flex-column jumbotron align-items-center justify-content-center text-center">
-      <img class="my-1" src="{{ Vite::asset('resources/img/laravel.png') }}" alt="">
+      <img class="my-1" src="{{ Vite::asset('resources/img/logo.png') }}" alt="">
       <h1 class="my-1">
         Laravel 9
         <span class="fs-6 fw-light text-muted"> - Bootstrap Template</span>
